10182946ce93a69189478f9fc6206d8ae3c6bacd
NitishKumarGundapu <nitishkumargundapu@gmail.com>
2021-09-16 18:27:08 +0530

    deleted test

--- test.py
+++
@@ -1,8 +0,0 @@
-from keras.models import model_from_json
-
-json_file=open(r"models/dark_s.json","r")
-loaded_model_json=json_file.read()
-json_file.close()
-loaded_model=model_from_json(loaded_model_json)
-loaded_model.load_weights("models/dark_s.h5")
-print("yes")

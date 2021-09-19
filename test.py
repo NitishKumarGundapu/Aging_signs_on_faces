f2d94f71f5acc4a9d5d0974e6c8e656a8c3bbd68
NitishKumarGundapu <nitishkumargundapu793@gmail.com>
2021-09-19 13:11:12 +0530

    just pulled onto device

--- test.py
+++ test.py
@@ -12,11 +12,11 @@ u2 = Image.open("images/images_2.jpg")
 u2 = u2.resize((450,450),Image.ANTIALIAS)
 u2 = ImageTk.PhotoImage(u2)
 Label(rootg,image=u2).place(x=0, y=0, width=450, height=450)
-
+'''
 img = cv2.resize(Imgarr0, (300, 250))
 im = Image.fromarray(img)
 imgtk = ImageTk.PhotoImage(image=im) 
 Label(rootg, image=imgtk).place(x=70,y=50)
-
+'''
 Label(rootg, text = "Predictions are : "+str(l)).place(x=50,y=350)
 rootg.mainloop()

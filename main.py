a1412f7aeaa1255b7c74d643f8403c652ff97f8b
NitishKumarGundapu <79395981+NitishKumarGundapu@users.noreply.github.com>
2021-09-19 20:00:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ json_file.close()
 loaded_model2=model_from_json(loaded_model_json)
 loaded_model2.load_weights("models/wrinkl.h5")
 
-a = input("enter the Image name : ")
+a = "56.jpg"
 Imgarr0 = cv2.imread(r"test_images/"+str(a))
 Imgarr1=cv2.resize(Imgarr0,(50,50))
 Imgarr = Imgarr1.reshape(-1, 50, 50, 3)
